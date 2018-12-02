f6451617a32e35e6f76ee5b8eda4bf44a61be72f
Amy Finkbiner <amyprime.camouflage@gmail.com>
2018-12-02 18:26:28 -0500

    Format contact info nicely

--- legislator-dashboard.js
+++ legislator-dashboard.js
@@ -53,30 +53,57 @@ function displayInfo() {
 // TODO: draw this explicitly rather than as a table
 function drawLegislator(chamber, district) {
   var tableId;
+  var title;
   switch (chamber) {
     case 'Senate':
       tableId = '1_QM0a4pAFRAO_5_TfO1-Rhs3n1Qm5mLGfsDhW-RC';
+	  title = "Senator";
       break;
     case 'House':
       tableId = '1EvdE9QCu0cJsOKKqI0nsQja5_GzKuxoQ8l8u7q71';
+	  title = "Representative";
       break;
     default:
       throw new "Chamber must be 'Senate' or 'House', not '" + chamber + "'.";
   }
   var query = new google.visualization.Query('https://www.google.com/fusiontables/gvizdata?tq=');
   query.setQuery(
-    "SELECT 'Full Name', 'Party'" +
+    "SELECT 'Last Name', 'Party', 'Freshman', 'District Phone', 'Capitol Phone', 'Email', 'Facebook', 'Twitter', 'Capitol Address', 'District Address 1'" +
     " FROM " + tableId +
     " WHERE 'District'=" + district);
-  query.send(drawLegislatorSub);
+  query.send(function (response) { drawLegislatorSub(response, title) });
 }
 
-function drawLegislatorSub(response) {
-  var data = response.getDataTable();
-  //var formatter = new google.visualization.NumberFormat({pattern: '##%'});
-  //formatter.format(data, 2);
-  var table = new google.visualization.Table(document.getElementById('legislator_div'));	
-  table.draw(data);
+function drawLegislatorSub(response, title) {
+  var dataTable = response.getDataTable();
+  var content;
+  if (dataTable.getNumberOfRows() != 1)
+  {
+	content = "Could not find contact info";
+  } else {
+    var lastName = dataTable.getValue(0, 0);
+    var party = dataTable.getValue(0, 1);
+    var freshman = dataTable.getValue(0, 2) == "Yes";
+    var districtPhone = dataTable.getValue(0, 3);
+    var capitolPhone = dataTable.getValue(0, 4);
+    var email = dataTable.getValue(0, 5);
+    var facebook = dataTable.getValue(0, 6);
+    var twitter = dataTable.getValue(0, 7);
+    var capitolAddress = dataTable.getValue(0, 8);
+    var districtAddress1 = dataTable.getValue(0, 9);
+
+    var content = "<strong>" + title + " " + lastName + " (" + party + ")</strong><br>";
+    if (freshman) { content = content + "<i>Newly elected; will take office Jan. 1st, 2019</i><br>"; }
+    content = content + "<br>";
+    if (districtPhone) { content = content + "District Phone: <a href='tel:1" + districtPhone.replace(/[^\d]/g,"") + "'>" + districtPhone + "</a><br>"; }
+    if (districtAddress1) { content = content + "District Address: " + districtAddress1 + "<br><br>"; }
+    if (capitolPhone) { content = content + "Capitol Phone: <a href='tel:1" + capitolPhone.replace(/[^\d]/g,"") + "'>" + capitolPhone + "</a><br>"; }
+    if (capitolAddress) { content = content + "Capitol Address: " + capitolAddress + "<br><br>"; }
+    if (email) { content = content + "Email: <a href='mailto:" + email + "'>" + email + "</a><br>"; }
+    if (facebook) { content = content + "Facebook: <a href='https://www.facebook.com/" + facebook.substring(1) + "'>" + facebook + "</a><br>"; }
+    if (twitter) { content = content + "Twitter: <a href='https://www.twitter.com/" + twitter.substring(1) + "'>" + twitter + "</a><br>"; }
+  }
+  document.getElementById('legislator_div').innerHTML = content;
 }
 	  
 function drawContacts(chamber, district) {
@@ -93,13 +120,13 @@ function drawContactsSub(response) {
   var data = response.getDataTable();
   var nRows = data.getNumberOfRows();
   if (nRows == 0) {
-    document.getElementById('contacts_message').innerHTML = "<br>This legislator hasn't been contacted yet!";
+    document.getElementById('contacts_message').innerHTML = "<strong>This legislator hasn't been contacted yet.</strong>";
     document.getElementById('contacts_table').innerHTML = "";
   } else {
     if (nRows == 1) {
-      document.getElementById('contacts_message').innerHTML = "<br>This legislator has been contacted once:";
+      document.getElementById('contacts_message').innerHTML = "<strong>This legislator has been contacted once:</strong>";
     } else {
-      document.getElementById('contacts_message').innerHTML = "<br>This legislator has been contacted " + nRows + " times:";
+      document.getElementById('contacts_message').innerHTML = "<strong>This legislator has been contacted " + nRows + " times:</strong>";
     }
     var table = new google.visualization.Table(document.getElementById('contacts_table'));	
     table.draw(data);
