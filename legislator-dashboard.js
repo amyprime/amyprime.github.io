a89d8d124d00484ed52b7f00796d8832a4441426
Amy Finkbiner <amyprime.camouflage@gmail.com>
2019-01-20 14:46:34 -0500

    Update legislator-dashboard.js

--- legislator-dashboard.js
+++ legislator-dashboard.js
@@ -1,6 +1,4 @@
-var namesSheetId = '1-nfLcspdj2h8rk07I7LQzUKG3oEV3r6tpjmL8p1hLrs';
-var contactsTableId = '1Y1HHWA-g4m9wOKkK5aJcLeRIG-ECCgc2cj2EG5SU';
-var formId = '1FAIpQLSfcSPNmIBfEDph61K1-6IJpvN8ULEHb8uKLuQ5x0Y-EWLkseA';
+// Load legislator-dashboard-setup-XYZ.js before the script to initialize global variables
 
 google.charts.load(
   'current',
