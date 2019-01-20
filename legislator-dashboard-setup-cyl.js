b101951568eb8aab8f8ff48588b9c4409a3527d9
Amy Finkbiner <amyprime.camouflage@gmail.com>
2019-01-20 14:43:33 -0500

    Create legislator-dashboard-setup-cyl.js

---
+++ legislator-dashboard-setup-cyl.js
@@ -0,0 +1,3 @@
+var namesSheetId = '1lg1A3DNPOzkPebrOEcKKOd4f1QX0lw2SST3oJZFOmDg';
+var contactsTableId = '1zJAsvZ804dOhYdHFPyCOfqvMuB2KKhpuQuJUi7wV';
+var formId = '1FAIpQLSfcinNG6GVH3QTTBIZjkW5-SK7kX362PdHzEmIVYLBnuipj6g';
