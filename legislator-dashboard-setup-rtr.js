44a6875677d86507b60fdd7652287b8e146e85a8
Amy Finkbiner <amyprime.camouflage@gmail.com>
2019-03-10 11:46:17 -0400

    Delete legislator-dashboard-setup-rtr.js

--- legislator-dashboard-setup-rtr.js
+++
@@ -1,3 +0,0 @@
-var namesSheetId = '1-nfLcspdj2h8rk07I7LQzUKG3oEV3r6tpjmL8p1hLrs';
-var contactsTableId = '1Y1HHWA-g4m9wOKkK5aJcLeRIG-ECCgc2cj2EG5SU';
-var formId = '1FAIpQLSfcSPNmIBfEDph61K1-6IJpvN8ULEHb8uKLuQ5x0Y-EWLkseA';
